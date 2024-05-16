f0690e6c73aa14f8cf67f584532eddbc7290a978
Mally13 <mallymutuku13@gmail.com>
2024-05-16 16:53:42 +0300

    Task 0:Regex-ing la

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,10 +5,11 @@ message obfuscated
 """
 import re
 import logging
+from typing import List
 
 
 
-def filter_datum(fields: list[str], redaction: str, message: str,
+def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """Returns a obfuscated log message"""
     for f in fields:
