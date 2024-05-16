bffa240ae8c7d86c1d403d7ce1d1dc43653bc96a
Mally13 <mallymutuku13@gmail.com>
2024-05-16 16:27:00 +0300

    Task 0:Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,5 +6,6 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
+    """Returns a obfuscated log message"""
     return re.sub(rf"({'|'.join(fields)})=[^{separator}]*",
-                   lambda match: match.group(1) + "=" + redaction, message)
+                  lambda match: match.group(1) + "=" + redaction, message)
