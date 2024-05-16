6a827d6c4f1b67e525eaa6e5d8ed380a804019b8
Mally13 <mallymutuku13@gmail.com>
2024-05-16 16:59:01 +0300

    Task 0:Regex-ing la

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,26 +9,9 @@ from typing import List
 
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
+def filter_datum(fields, redaction, message, separator):
     """Returns a obfuscated log message"""
     for f in fields:
         message = re.sub(rf"{f}=(.*?)\{separator}",
                             f'{f}={redaction}{separator}', message)
     return message
-    
-
-
-class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-        """
-
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self):
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-
-    def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
