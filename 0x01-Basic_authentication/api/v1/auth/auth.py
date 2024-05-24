7fc86a453a19bdddeeab2e280670994b97c5f29e
Mally13 <mallymutuku13@gmail.com>
2024-05-24 11:57:37 +0300

    Task 6: Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+""" Manages API authentication
+"""
+from flask import request
+from typing import List, TypeVar
+from models.user import User
+
+
+class Auth:
+    """A class to manage the API authentication"""
+    def __init__(self) -> None:
+        """initializes the auth class"""
+        pass
+
+    def require_auth(self, path: str, exclude_paths: List[str]) -> bool:
+        """For paths that require auth"""
+        if path is None:
+            return True
+        elif not exclude_paths:
+            return True
+        else:
+            path = path.strip()
+            if path[-1] != '/':
+                path += '/'
+            for excluded_path in exclude_paths:
+                excluded_path = excluded_path.strip()
+                if path.lower() == excluded_path.lower():
+                    return False
+                else:
+                    return True
+
+    def authorization_header(self, request=None) -> str:
+        """Handles flask request object"""
+        if request is None:
+            return None
+        elif 'Authorization' in request.headers:
+            return request.headers.get('Authorization')
+        else:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user"""
+        return None
