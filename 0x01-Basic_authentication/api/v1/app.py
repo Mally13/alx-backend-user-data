3822246371c2220696bb8615f7d4fb0f0c298c85
Mally13 <mallymutuku13@gmail.com>
2024-05-24 12:49:17 +0300

    Task 6: Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,7 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
-from api.v1.auth.basic_auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -61,7 +61,7 @@ def before_request_handler():
 if __name__ == "__main__":
     auth = getenv("AUTH_TYPE")
     if auth:
-        auth = Auth()
+        auth = BasicAuth()
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
     app.run(host=host, port=port, debug=True)
--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,40 +4,9 @@
 from flask import request
 from typing import List, TypeVar
 from models.user import User
+from api.v1.auth.auth import Auth
 
 
-class Auth:
+class BasicAuth(Auth):
     """A class to manage the API authentication"""
-    def __init__(self) -> None:
-        """initializes the auth class"""
-        pass
-
-    def require_auth(self, path: str, exclude_paths: List[str]) -> bool:
-        """For paths that require auth"""
-        if path is None:
-            return True
-        elif not exclude_paths:
-            return True
-        else:
-            path = path.strip()
-            if path[-1] != '/':
-                path += '/'
-            for excluded_path in exclude_paths:
-                excluded_path = excluded_path.strip()
-                if path.lower() == excluded_path.lower():
-                    return False
-                else:
-                    return True
-
-    def authorization_header(self, request=None) -> str:
-        """Handles flask request object"""
-        if request is None:
-            return None
-        elif 'Authorization' in request.headers:
-            return request.headers.get('Authorization')
-        else:
-            return None
-
-    def current_user(self, request=None) -> TypeVar('User'):
-        """current user"""
-        return None
+    pass
