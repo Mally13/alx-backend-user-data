0fc345be622330fe85a89bbc5bbb040197f74f09
Mally13 <mallymutuku13@gmail.com>
2024-05-27 00:14:36 +0300

    Documentation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -56,14 +56,14 @@ def get_logger() -> logging.Logger:
 
 def get_db() -> mysql.connector.connection.MySQLConnection:
     """Establishes a database connection"""
-    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
-    user = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
     password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    host = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
     database = os.getenv('PERSONAL_DATA_DB_NAME')
     try:
         personal_db = mysql.connector.connect(
             host=host,
-            user=user,
+            user=username,
             database=database,
             password=password
         )
